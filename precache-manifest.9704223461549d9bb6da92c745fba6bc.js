75eff5dd0f4f96af7954e2e1786ae1bdb2b17cb3
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-27 00:20:07 +0600

    Updates

---
+++ precache-manifest.9704223461549d9bb6da92c745fba6bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20049ccc90981230f68ad68aa183d6f9",
+    "url": "/contextAPI-hook-cart/index.html"
+  },
+  {
+    "revision": "3be3f56220ff15b463e6",
+    "url": "/contextAPI-hook-cart/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3be3f56220ff15b463e6",
+    "url": "/contextAPI-hook-cart/static/js/2.4332bdfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contextAPI-hook-cart/static/js/2.4332bdfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f14648eb1e7aeb5d1c29",
+    "url": "/contextAPI-hook-cart/static/js/main.bc97946c.chunk.js"
+  },
+  {
+    "revision": "8dfae6272696f37b6b59",
+    "url": "/contextAPI-hook-cart/static/js/runtime-main.e8c86469.js"
+  }
+]);
