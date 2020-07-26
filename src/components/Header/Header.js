43ea72476dc2409d18f37ffd205b10200bfb1e19
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-27 00:13:20 +0600

    cart done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProductContext } from "../../Context/Context";
 
 const Header = () => {
+  const [cart, setCart] = useContext(ProductContext);
+  const totalPrice = cart.reduce((acc, curr) => acc + curr.price, 0);
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
       <ul className="navbar-nav m-auto">
         <li className="nav-item">
           <span style={{ fontWeight: "bold", fontSize: "20px" }}>
-            Total Price - 0 Cart - 0
+            Total Price - {totalPrice} Cart - {cart.length}
           </span>
         </li>
       </ul>
